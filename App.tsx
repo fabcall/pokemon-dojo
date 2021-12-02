8f3a546c794b150dd34bb1e3ee16fecbd47b1421
Fábio Calliari <fabio.calliari@nutrien.com>
2021-12-02 19:56:00 -0300

    feat: setup main stack

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
 import React, {useEffect} from 'react';
+import {Text, View} from 'react-native';
 
 import BootSplash from 'react-native-bootsplash';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 const delay = (timeout: number) =>
   new Promise(resolve => setTimeout(resolve, timeout));
+
+const HomeScreen = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+const Stack = createStackNavigator();
 
 const App = () => {
   useEffect(() => {
@@ -17,7 +30,13 @@ const App = () => {
   }, []);
 
   return (
-    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+    <GestureHandlerRootView>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 };
 
