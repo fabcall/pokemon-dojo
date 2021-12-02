ac90d893a5012afd04397b60b731d23a46b8c8ec
Fábio Calliari <fabio.calliari@nutrien.com>
2021-12-02 20:26:42 -0300

    feat: add main stack types

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,11 @@
 import {Text, View} from 'react-native';
 
 import React from 'react';
+import {StackScreenProps} from '@react-navigation/stack';
 
-const Home = () => {
+type Props = StackScreenProps<ReactNavigation.RootParamList, 'Home'>;
+
+const Home = ({}: Props) => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
--- src/routes/RootStack/index.tsx
+++ src/routes/RootStack/index.tsx
@@ -2,6 +2,16 @@ import Home from '../../pages/Home';
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 
+type RootStackParamList = {
+  Home: undefined;
+};
+
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
+}
+
 const Stack = createStackNavigator();
 
 const RootStack = () => {
